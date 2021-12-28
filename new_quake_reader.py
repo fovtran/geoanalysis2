e7d54088e8ad1f80d81c4a9817f889dc53add53f
diego2 <vivenentitan@hotmail.com>
2021-12-28 00:39:21 -0300

    multiple updates 27-12-2021

--- new_quake_reader.py
+++ new_quake_reader.py
@@ -22,12 +22,24 @@ df2 = pd.DataFrame( {'Vents': ['Vent 1'], 'Name': ['Dolly'], 'latitud': [28.58],
 import mapclassify as mc
 
 start_date = "2021-12-5"
-end_date = "2021-12-12"
+end_date = "2021-12-27"
 df1 = SeriesCutByDate(start_date, end_date, df)
 m = mc.MaximumBreaks(df1.magnitud, k=8)
 p1 = mc.MaximumBreaks(df1.profundidad, k=12, mindiff=4)
-print(m)
+
+#print(m)
 print(p1)
+#print(p1.get_tss())
+#print(p1.get_adcm())
+#print(p1.get_gadf())
+print(p1.get_legend_classes())
+print(p1.counts)
+print(dir(p1))
+#print(p1.get_fmt())
+#print(p1.bins)
+#print(p1.y)
+#print(p1.yb)
+#print(p1.table())
 
 start_date = "2021-10-1"
 end_date = "2021-12-13"
@@ -39,8 +51,8 @@ p = dates['profundidad'].agg(calc)
 #print(m['max']>3.5)
 e = pd.DataFrame({'Date':dates.size()})
 
-e = (dates.groupby(['magnitud', 'profundidad']).agg({
-    'magnitud': ['mean', 'count'], 
-    'profundidad': ['median', 'min', 'count']
-    }))
-print(e)
+#e = (dates.groupby(['magnitud', 'profundidad']).agg({
+#    'magnitud': ['mean', 'count'],
+#    'profundidad': ['median', 'min', 'count']
+#    }))
+#print(e)
